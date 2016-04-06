45351f916ea54f6881fa8a968bd10853106f7d43
mattwilliams85 <mattwilliams85@gmail.com>
2016-04-06 09:22:46 -0700

    adds navbar events

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  // NAV POSITION
+  var navPos = $('nav').position().top;
+  var lastPos = 0;
+
+  $(window).on('scroll', function () {
+    var pos = $(window).scrollTop();
+
+    if (pos >= navPos + $('nav').height() && lastPos < pos) {
+      $('nav').addClass('fixed');
+    }
+    if (pos < navPos && lastPos > pos) {
+      $('nav').removeClass('fixed');
+    }
+    lastPos = pos;
+  });
+
+  // PAGE ANCHORS
+  $('nav span').click(function(){
+    var anchor = $(this).text();
+    $('nav span').removeClass('active');
+    $(this).addClass('active');
+
+    $('html, body').animate({
+        scrollTop: $('#' + anchor).offset().top
+    }, 300);
+  });
+});
